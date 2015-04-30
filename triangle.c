25b2b38b0d5cfc9fbe6552bc3c62b2bd5f107ec5
Simon Eriksson <simer882@student.liu.se>
2015-04-30 18:38:26 +0200

    Parameter values displayed, lines now below the triangle, some cleanup

--- triangle.c
+++ triangle.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <stdarg.h>
 #include <stdint.h>
 #include <stdbool.h>
 #include <libdragon.h>
@@ -5,22 +7,27 @@
 static resolution_t res = RESOLUTION_320x240;
 static bitdepth_t bit = DEPTH_32_BPP;
 
+#define WIDTH 320
+#define HEIGHT 240
+
 #define FIXED_11_2(i,f) ((((int16_t)i) << 2) | f)
 #define FIXED_16_16(i,f) ((((int16_t)i) << 16) | f)
 
 #define INTPART_11_2(x) (x >> 2)
 #define INTPART_16_16(x) (x >> 16)
 
+#define TOFLOAT_16_16(x) (x / 65536.0)
+
 bool major = 1;
-uint32_t yl = FIXED_11_2(150,0);
-uint32_t ym = FIXED_11_2(50,0);
-uint32_t yh = FIXED_11_2(50,0);
-uint32_t xl = FIXED_16_16(150,0);
-uint32_t dxldy = FIXED_16_16(-1,0);
-uint32_t xh = FIXED_16_16(50,0);
-uint32_t dxhdy = FIXED_16_16(0,0);
-uint32_t xm = FIXED_16_16(50,0);
-uint32_t dxmdy = FIXED_16_16(0,0);
+int32_t yl = FIXED_11_2(150,0);
+int32_t ym = FIXED_11_2(50,0);
+int32_t yh = FIXED_11_2(50,0);
+int32_t xl = FIXED_16_16(150,0);
+int32_t dxldy = FIXED_16_16(-1,0);
+int32_t xh = FIXED_16_16(50,0);
+int32_t dxhdy = FIXED_16_16(0,0);
+int32_t xm = FIXED_16_16(50,0);
+int32_t dxmdy = FIXED_16_16(0,0);
 
 enum {
 	YL,
@@ -48,6 +55,17 @@ void rdp_send(uint32_t *data, int length){
 
 uint32_t triangle_commands[8] __attribute__ ((aligned (8)));
 
+void graphics_printf(display_context_t disp, int x, int y, char *szFormat, ...){
+	char szBuffer[64];
+
+	va_list pArgs;
+	va_start(pArgs, szFormat);
+	vsnprintf(szBuffer, sizeof szBuffer, szFormat, pArgs);
+	va_end(pArgs);
+
+	graphics_draw_text(disp, x, y, szBuffer);
+}
+
 int main(void){
 	static display_context_t disp = 0;
 
@@ -72,20 +90,29 @@ int main(void){
 		graphics_fill_screen(disp,0x000000FF);
 
 		switch(curvar){
-			case YL: graphics_draw_text(disp,20,20,"YL"); break;
-			case YM: graphics_draw_text(disp,20,20,"YM"); break;
-			case YH: graphics_draw_text(disp,20,20,"YH"); break;
-			case XL: graphics_draw_text(disp,20,20,"XL"); break;
-			case XM: graphics_draw_text(disp,20,20,"XM"); break;
-			case XH: graphics_draw_text(disp,20,20,"XH"); break;
-			case DXLDY: graphics_draw_text(disp,20,20,"DxLDy"); break; 
-			case DXMDY: graphics_draw_text(disp,20,20,"DxMDy"); break; 
-			case DXHDY: graphics_draw_text(disp,20,20,"DxHDy"); break;
+			case YL: graphics_printf(disp,20,20,"YL %d", INTPART_11_2(yl)); break;
+			case YM: graphics_printf(disp,20,20,"YM %d", INTPART_11_2(ym)); break;
+			case YH: graphics_printf(disp,20,20,"YH %d", INTPART_11_2(yh)); break;
+			case XL: graphics_printf(disp,20,20,"XL %d", INTPART_16_16(xl)); break;
+			case XM: graphics_printf(disp,20,20,"XM %d", INTPART_16_16(xm)); break;
+			case XH: graphics_printf(disp,20,20,"XH %d", INTPART_16_16(xh)); break;
+			case DXLDY: graphics_printf(disp,20,20,"DxLDy %.2f", TOFLOAT_16_16(dxldy)); break;
+			case DXMDY: graphics_printf(disp,20,20,"DxMDy %.2f", TOFLOAT_16_16(dxmdy)); break;
+			case DXHDY: graphics_printf(disp,20,20,"DxHDy %.2f", TOFLOAT_16_16(dxhdy)); break;
 			default: break;
-
 		}
 
-		graphics_draw_text(disp, 260, 20, major ? "Right" : "Left");
+		//Y lines
+		graphics_draw_line(disp,0,INTPART_11_2(yh),WIDTH-1,INTPART_11_2(yh),0xFFFF0000);
+		graphics_draw_line(disp,0,INTPART_11_2(ym),WIDTH-1,INTPART_11_2(ym),0xFF800000);
+		graphics_draw_line(disp,0,INTPART_11_2(yl),WIDTH-1,INTPART_11_2(yl),0xFF000000);
+
+		//X lines
+		graphics_draw_line(disp,INTPART_16_16(xh),0,INTPART_16_16(xh),HEIGHT-1,0x00FFFF00);
+		graphics_draw_line(disp,INTPART_16_16(xm),0,INTPART_16_16(xm),HEIGHT-1,0x0080FF00);
+		graphics_draw_line(disp,INTPART_16_16(xl),0,INTPART_16_16(xl),HEIGHT-1,0x0000FF00);
+
+		graphics_printf(disp, 260, 20, major ? "Right" : "Left");
 
 		rdp_sync(SYNC_PIPE);
 		rdp_set_default_clipping();
@@ -97,16 +124,6 @@ int main(void){
 		rdp_send(triangle_commands,sizeof triangle_commands);
 		rdp_sync(SYNC_PIPE);
 		rdp_detach_display();
-
-		//Y lines
-		graphics_draw_line(disp,0,INTPART_11_2(yh),320,INTPART_11_2(yh),0xFFFF0000);
-		graphics_draw_line(disp,0,INTPART_11_2(ym),320,INTPART_11_2(ym),0xFF800000);
-		graphics_draw_line(disp,0,INTPART_11_2(yl),320,INTPART_11_2(yl),0xFF000000);
-
-		//X lines
-		graphics_draw_line(disp,INTPART_16_16(xh),0,INTPART_16_16(xh),240,0x00FFFF00);
-		graphics_draw_line(disp,INTPART_16_16(xm),0,INTPART_16_16(xm),240,0x0080FF00);
-		graphics_draw_line(disp,INTPART_16_16(xl),0,INTPART_16_16(xl),240,0x0000FF00);
 
 		display_show(disp);
 
@@ -124,30 +141,30 @@ int main(void){
 		}
 		else if(keys.c[0].right){
 			switch(curvar){
-				case YL: yl += 4; break;
-				case YM: ym += 4; break;
-				case YH: yh += 4; break;
-				case XL: xl += 65536; break;
-				case XM: xm += 65536; break;
-				case XH: xh += 65536; break;
-				case DXLDY: dxldy += 16384; break;
-				case DXMDY: dxmdy += 16384; break;
-				case DXHDY: dxhdy += 16384; break;
+				case YL: yl += FIXED_11_2(1,0); break;
+				case YM: ym += FIXED_11_2(1,0); break;
+				case YH: yh += FIXED_11_2(1,0); break;
+				case XL: xl += FIXED_16_16(1,0); break;
+				case XM: xm += FIXED_16_16(1,0); break;
+				case XH: xh += FIXED_16_16(1,0); break;
+				case DXLDY: dxldy += FIXED_16_16(0,16384); break;
+				case DXMDY: dxmdy += FIXED_16_16(0,16384); break;
+				case DXHDY: dxhdy += FIXED_16_16(0,16384); break;
 				default: break;
 
 			}
 		}
 		else if(keys.c[0].left){
 			switch(curvar){
-				case YL: yl -= 4; break;
-				case YM: ym -= 4; break;
-				case YH: yh -= 4; break;
-				case XL: xl -= 65536; break;
-				case XM: xm -= 65536; break;
-				case XH: xh -= 65536; break;
-				case DXLDY: dxldy -= 16384; break;
-				case DXMDY: dxmdy -= 16384; break;
-				case DXHDY: dxhdy -= 16384; break;
+				case YL: yl -= FIXED_11_2(1,0); break;
+				case YM: ym -= FIXED_11_2(1,0); break;
+				case YH: yh -= FIXED_11_2(1,0); break;
+				case XL: xl -= FIXED_16_16(1,0); break;
+				case XM: xm -= FIXED_16_16(1,0); break;
+				case XH: xh -= FIXED_16_16(1,0); break;
+				case DXLDY: dxldy -= FIXED_16_16(0,16384); break;
+				case DXMDY: dxmdy -= FIXED_16_16(0,16384); break;
+				case DXHDY: dxhdy -= FIXED_16_16(0,16384); break;
 				default: break;
 			}
 		}
